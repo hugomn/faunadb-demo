3aab3864569743ce0530454f37220821619f70ce
Hugo Nogueira <hugomn@gmail.com>
2020-05-14 13:37:01 +0200

    Add countries

---
+++ scripts/countries.js
@@ -0,0 +1,44 @@
+const faunadb = require("faunadb");
+const secret = process.env.FAUNADB_SECRET_KEY;
+const q = faunadb.query;
+const client = new faunadb.Client({ secret });
+const countries = require("../data/countries.json");
+
+const printCountries = async () => {
+  try {
+    const result = await client.query(
+      q.Map(
+        q.Paginate(
+          q.Match(
+            q.Index("all_countries") // specify source
+          )
+        ),
+        (ref) => q.Get(ref)
+      )
+    );
+    console.log(result.data);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const insertCountries = async () => {
+  try {
+    for (const country of countries) {
+      const result = await client.query(
+        q.Create(q.Collection("countries"), {
+          data: {
+            code: country.code,
+            name: country.name,
+          },
+        })
+      );
+      console.log(result.data);
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+// printCountries();
+insertCountries();
